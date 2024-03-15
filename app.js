4cac96b7b3aa059cd28c44d5e3f611aea157cb3e
varshini <2116327@cognizant.com>
2024-03-15 15:03:54 +0530

    Changes added

--- app.js
+++ app.js
@@ -1,13 +1,55 @@
-// const heading = React.createElement('h1', {
-//     id: 'heading',
-//     abc: 'ab'}, 'Hello from React!');
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(heading);
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import HeadingComponent from './Components/HeadingComponent';
+import Body from './Components/BodyComponent';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import { useState } from 'react';
+import ProductDescription from './Components/ProductDescription';
+import AboutUs from "./Components/AboutUs";
+import ContactUs from "./Components/ContactUs";
+import { appStore } from './utils/appStore';
+import { Provider } from 'react-redux';
+import Cart from './Components/cart';
 
-const parent = React.createElement('div', {id: 'parent'}, 
-    React.createElement('div', {id: 'child'}, 
-    [React.createElement('h1', {}, 'Im h1 tag'), React.createElement('h1', {}, 'Im h1 tags sibling ')]));
+let MyApp = () => {
+    return (
+        <div>
+            <Provider store={appStore}>
+                <HeadingComponent />
+                <Outlet />
+            </Provider>
+        </div>
+    );
+}
 
-console.log(parent);
+const appRouter = createBrowserRouter ([
+    {
+        path: '/',
+        element: <MyApp />,
+        children: [
+            {
+                path: '/',
+                element: <Body />
+            },
+            {
+                path: '/products/:productId',
+                element: <ProductDescription />
+            },
+            {
+                path: '/about',
+                element: <AboutUs />
+            },
+            {
+                path: '/contact',
+                element: <ContactUs />
+            },
+            {
+                path: '/cart.jsx',
+                element: <Cart />
+            }
+        ]
+    }
+])
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+root.render(<RouterProvider router={appRouter} />);
